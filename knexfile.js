5b3073a8c55badab485ba8eb445bd34fdbb70de8
johmert <johmert@users.noreply.github.com>
2022-02-15 13:57:58 -0800

    Knex configuration

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const { DATABASE_URL } = process.env;
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: DATABASE_URL,
+  }
+};
