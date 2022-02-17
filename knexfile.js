06cdfe58b917c02aa638a8cdbe00571e4694bc65
johmert <johmert@users.noreply.github.com>
2022-02-17 14:20:58 -0800

    seeding in knex

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,16 @@
+const path = require("path");
 require("dotenv").config();
 const { DATABASE_URL } = process.env;
 
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
 module.exports = {
   development: {
-    client: 'postgresql',
-    connection: DATABASE_URL,
-  }
+    client: "postgresql",
+    connection: "postgres://hejwbcuf:tgH-NJhL3dGCdHdNTwqr9MS9S9VWLgce@kashin.db.elephantsql.com/hejwbcuf",
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
 };
--- src/db/connection.js
+++ src/db/connection.js
@@ -1,4 +1,4 @@
-const env = process.env.NOD_ENV || "development";
+const env = process.env.NODE_ENV || "development";
 const config = require("../../knexfile")[env];
 const knex = require("knex")(config);
 
